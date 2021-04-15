bb296b02284bfaefb536c26e8e39ca769b72acd3
lygttpod <lygttpod@163.com>
2021-04-15 10:39:15 +0800

    封装APP状态管理组件

--- lib/widget/app_page_status_widget.dart
+++ lib/widget/app_page_status_widget.dart
@@ -2,39 +2,58 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_wan_android/config/page_status.dart';
 
+class AppPageStatusWidget extends StatelessWidget {
+  final PageStatus pageStatus;
+  final Widget child;
 
-Widget appPageStatusWidget(PageStatus pageStatus, Widget successWidget) {
-  switch (pageStatus) {
-    case PageStatus.LOADING:
-      return appLoadingPage();
-      break;
-    case PageStatus.ERROR:
-      return appErrorPage();
-      break;
-    case PageStatus.EMPTY:
-      return appEmptyPage();
-      break;
-    case PageStatus.SUCCESS:
-      return successWidget;
-      break;
-    default:
-      return appEmptyPage();
+  final Function onReplayBtnPressed;
+
+  const AppPageStatusWidget({this.child, this.pageStatus, this.onReplayBtnPressed}) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return appPageStatusWidget(child, pageStatus ?? PageStatus.SUCCESS, onReplayBtnPressed);
   }
-}
 
-Widget appErrorPage() {
-  return Center(
-    child: Text("页面出错啦"),
-  );
-}
+  Widget appPageStatusWidget(Widget successWidget,PageStatus pageStatus,  Function onReplayBtnPressed) {
+    switch (pageStatus) {
+      case PageStatus.LOADING:
+        return appLoadingPage();
+        break;
+      case PageStatus.ERROR:
+        return appErrorPage(onReplayBtnPressed);
+        break;
+      case PageStatus.EMPTY:
+        return appEmptyPage();
+        break;
+      case PageStatus.SUCCESS:
+        return successWidget;
+        break;
+      default:
+        return appEmptyPage();
+    }
+  }
 
-Widget appEmptyPage() {
-  return Center(
-    child: Text("暂无数据"),
-  );
-}
-Widget appLoadingPage() {
-  return Center(
-    child: CircularProgressIndicator(),
-  );
+  Widget appErrorPage(Function onReplayBtnPressed) {
+    return Center(
+      child: Column(
+        children: [
+          Text("页面出错啦"),
+          ElevatedButton(onPressed: onReplayBtnPressed, child: Text("点击重试"))
+        ],
+      ),
+    );
+  }
+
+  Widget appEmptyPage() {
+    return Center(
+      child: Text("暂无数据"),
+    );
+  }
+
+  Widget appLoadingPage() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
 }
